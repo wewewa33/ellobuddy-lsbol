baaa9b1e9fef7a583db81aba86d52fb3d3e90e49
wewewa33 <wewewa555@gmail.com>
2016-03-21 19:28:18 +0700

    s

--- LS-Ulity/Skin/Skin-Elobuddy LS+/Program.cs
+++ LS-Ulity/Skin/Skin-Elobuddy LS+/Program.cs
@@ -119,7 +119,7 @@ namespace Skin
 
 				Text1.Position = Drawing.WorldToScreen(Player.Instance.Position) - new Vector2(280, -40);
                 Text1.Color = Color.LimeGreen;
-                Text1.TextValue = "✔✔✔ LS+ Faker - ViệtNamese -H4ckercfqq2 (Lêu Lêu mấy thằng Xài Tool mà gà :D)✔✔✔";
+                Text1.TextValue = "✔✔✔ LS+ Faker - ViệtNamese -H4ckercfqq2 (Lêu Lêu mấy thằng Xài Tool mà gà :D)✔✔✔" ,new Font("Euphemia", 10F);
                 Text1.Draw();
 				
                 Text1.Position = Drawing.WorldToScreen(Player.Instance.Position) - new Vector2(100, -60);
