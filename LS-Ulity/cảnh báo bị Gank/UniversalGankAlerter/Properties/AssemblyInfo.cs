02b5a5f27833908432af86897150b1989aa8069c
wewewa33 <wewewa555@gmail.com>
2016-03-24 22:47:56 +0700

    2

--- LS-Ulity/cảnh báo bị Gank/UniversalGankAlerter/Properties/AssemblyInfo.cs
+++ LS-Ulity/cảnh báo bị Gank/UniversalGankAlerter/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Universal Gank Alerter")]
+[assembly: AssemblyTitle("Cảnh báo rừng")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Universal Gank Alerter")]
+[assembly: AssemblyProduct("Cảnh báo rừng")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("LS+'1.0'")]
+[assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("LS+'1.0'")]
