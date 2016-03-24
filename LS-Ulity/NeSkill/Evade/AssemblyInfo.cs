c4fef77bb8eab7dd1a6e5b98c07a6b1b544f0066
wewewa33 <wewewa555@gmail.com>
2016-03-24 22:42:37 +0700

    2

--- LS-Ulity/NeSkill/Evade/AssemblyInfo.cs
+++ LS-Ulity/NeSkill/Evade/AssemblyInfo.cs
@@ -56,4 +56,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("5.16.0.1")]
-[assembly: AssemblyFileVersion("5.16.0.1")]
+[assembly: AssemblyFileVersion("1.0")]
